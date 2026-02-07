ecc634aec38c40908b963c0e085fdddb136a8c52
iborraxavi <50479651+iborraxavi@users.noreply.github.com>
2026-02-07 08:14:11 +0100

    feat: Create project structure

--- xis-prices-boot/src/main/java/com/xis/prices/PricesApplication.java
+++ xis-prices-boot/src/main/java/com/xis/prices/PricesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PricesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PricesApplication.class, args);
-	}
-
+  static void main(String[] args) {
+    SpringApplication.run(PricesApplication.class, args);
+  }
 }
